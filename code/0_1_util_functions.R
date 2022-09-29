711a6304837a16b2decf748da323550c86d1c7ca
Yang <yang.clara.liu@gmail.com>
2022-09-29 20:26:23 +0100

    flexible u and y updates by day

--- code/0_1_util_functions.R
+++ code/0_1_util_functions.R
@@ -21,18 +21,20 @@ cm_multinom_process <- function(
 }
 
 gen_country_basics <- function(country,
-                               waning_nat = 52*7*3,
                                R0_assumed  = 2.7,
                                date_start = "2020-01-01",
                                date_end = "2022-12-31",
                                processes = NULL,
+                               contact = contact_schedule,
+                               period_wn  = 3*365, # duration, waning of natural immunity
+                               period_wv_ml = 1*365, # duration, waning from medium to low levels vaccine induced 
                                deterministic = TRUE){
   
   require(countrycode)
   
   iso3c_tmp = countrycode(country, "country.name", "iso3c")
   
-  c_tmp = contact_schedule %>% 
+  c_tmp = contact %>% 
     filter(country_region == country) %>% 
     filter(date >= date_start,
            date <= date_end)
@@ -64,7 +66,8 @@ gen_country_basics <- function(country,
     para$pop[[i]]$u = para$pop[[i]]$u * R0_assumed / current_R0
     
     # natural waning
-    para$pop[[i]]$wn <- rep((1/waning_nat), n_age_groups)
+    para$pop[[i]]$wn <- rep((1/period_wn), 
+                            n_age_groups)
     
     ## Set seeds to control start of outbreak
     # infections start in individuals aged 20-50
@@ -90,31 +93,236 @@ gen_country_basics <- function(country,
       unname,
     times = 1:nrow(c_tmp))
   
+  # waning vaccine-induced immunity
+  para$pop[[1]]$wn <- rep(1/period_wn, n_age_groups)
+  para$pop[[1]]$wv_ml <-  rep(1/period_wv_ml, n_age_groups)
+  
   return(para)
 }
 
-update_vac_char <- function(para,
-                            ve_i_l  = NULL, # infection blocking VE among V_l
-                            ve_i_m  = NULL, # infection blocking VE among V_m
-                            ve_d_l  = NULL, # disease reducing VE among E_l
-                            ve_d_m  = NULL, # disease reducing VE among E_m 
-                            period_wn  = NULL, # duration, waning of natural immunity
-                            period_wv_ml = NULL # duration, waning from medium to low levels vaccine induced 
+
+# # generate some test data of what efficacy weights should look like
+# CJ(date = seq(ymd("2019-12-01"),
+#               ymd("2024-01-01"),
+#               by = "day")) %>%
+#   mutate(weights_ve_i_l = rnorm(nrow(.),1,0.05),
+#          weights_ve_i_m = rnorm(nrow(.),1,0.05),
+#          weights_ve_d_l = rnorm(nrow(.),1,0.05),
+#          weights_ve_d_m = rnorm(nrow(.),1,0.05)) -> efficacy_weights_test
+# write_rds(efficacy_weights_test,
+#           "data/intermediate/efficacy_weights_test.rds"
+#           )
+efficacy_weights_test <- read_rds("data/intermediate/efficacy_weights_test.rds")
+efficacy_weights_test |> 
+  mutate_at(vars(starts_with("weights")), function(x) x <- 1) -> efficacy_weights_one
+
+# this function will update u and y
+# there's two sources for u and y changes
+# (1) VOC emergences related to changes in susceptibility and clinical fraction
+# (2) different vaccine efficacy values intput at different time steps due to 
+# composition of vaccines
+update_u_y <- function(para,
+                       date_switch = c("2021-01-15", "2021-04-15", "2021-12-15"),
+                       rc_u = c(1, 1.5, 0.5), # relative changes in u
+                       rc_y = c(1, 0.5, 0.5), # relative changes in y
+                       rc_ve = c(1, 0.9, 0.7), # relative changes in ve
+                       efficacy_baseline = NULL, # vaccine efficacy 
+                       efficacy_weights = efficacy_weights_test # as a result of different vaccine composition
 ){
+  # debug
+  # date_switch = c("2021-01-15", "2021-04-15", "2021-12-15")
+  # rc_u = c(1, 1.5, 0.5)
+  # rc_y = c(1, 0.5, 0.5)
+  # rc_ve = c(1, 0.9, 0.7)
+  # efficacy_baseline = ve_az
+  # efficacy_weights = efficacy_weights_test
+
+  date_range <- c(lubridate::ymd(para$date0),
+                  date_switch,
+                  lubridate::ymd(para$date0) + para$time1)
+  t_range <- c(para$time0:para$time1)
+  targets <- data.frame(scaler_label = c("uv_l_scaler", "uv_m_scaler",
+                                         "yv_l_scaler", "yv_m_scaler"),
+                        variable_label = c("uv_l", "uv_m",
+                                           "yv_l", "yv_m"))
+  n_age_groups <- para$pop[[1]]$n_groups
   
-  n_age <- length(para$pop[[1]]$size)
+  # create modifier table
+  CJ(date = seq(lubridate::ymd(para$date0),
+                lubridate::ymd(para$date0) + para$time1,
+                by = "day")) |> 
+    mutate(phase = as.numeric(NA)) -> modifier
   
-  # key parameters on infection and disease blocking mechanisms
-  para$pop[[1]]$uv_l  <- para$pop[[1]]$u * (1 - ve_i_l)
-  para$pop[[1]]$uv_m <- para$pop[[1]]$u * (ve_i_m)
-  para$pop[[1]]$yv_l  <- para$pop[[1]]$y * (1 - ve_d_l)
-  para$pop[[1]]$yv_m <- para$pop[[1]]$y * (1 - ve_d_m)
+  for(i in 1:length(date_range)){
+    modifier[modifier$date >= date_range[i] & 
+               modifier$date <= date_range[i+1],"phase"] <- i
+    modifier[modifier$date >= date_range[i] & 
+               modifier$date <= date_range[i+1],"rc_u_prod"] <- prod(c(1,rc_u)[1:i])
+    modifier[modifier$date >= date_range[i] & 
+               modifier$date <= date_range[i+1],"rc_y_prod"] <- prod(c(1,rc_y)[1:i])
+    modifier[modifier$date >= date_range[i] & 
+               modifier$date <= date_range[i+1],"rc_ve_prod"] <- prod(c(1,rc_ve)[1:i])
+  }
   
-  # waning vaccine-induced immunity
-  para$pop[[1]]$wn <- rep(1/period_wn, n_age)
-  para$pop[[1]]$wv_ml <-  rep(1/period_wv_ml, n_age)
+  modifier |> 
+    left_join(efficacy_weights,
+              by = "date") |> 
+    mutate(uv_l_scaler = rc_u_prod*(1 - efficacy_baseline$ve_i_o[1]*weights_ve_i_l*rc_ve_prod),
+           uv_m_scaler = rc_u_prod*(1 - efficacy_baseline$ve_i_o[2]*weights_ve_i_m*rc_ve_prod),
+           yv_l_scaler = rc_y_prod*(1 - efficacy_baseline$ve_d[1]*weights_ve_d_l*rc_ve_prod),
+           yv_m_scaler = rc_y_prod*(1 - efficacy_baseline$ve_d[2]*weights_ve_d_m*rc_ve_prod)) -> modifier
+
+  modifier |> 
+    select(ends_with("scaler")) |> 
+    pivot_longer(targets$scaler_label) |> 
+    group_by(name) |> summarise(value = min(value)) |> 
+    pull(value) |> (function(y) y > 0)() |> all() -> test_range
+  
+  testthat::expect(test_range,
+                   failure_message = "zero scaler values generated. 
+                   please double check all rc_xx variables.")
+  
+  for(i in seq_len(nrow(targets))){
+    tmp <-   modifier |> 
+      pull(targets$scaler_label[i]) |> 
+      split(seq(nrow(modifier))) |> 
+      map(rep, n_age_groups) |> 
+      map(unname)
+    
+    para$schedule[[targets$scaler_label[i]]] <-  list(
+      parameter = targets$variable_label[i],
+      pops = numeric(),
+      mode = "multiply",
+      values = tmp,
+      times = t_range
+    )
+    
+    rm(tmp)
+  }
   
   # return results
   return(para)
 }
+
+# this function is reduced now to only make changes to only modify the health
+# system burden processes changed as a result of emerging VOC
+
+# emerge_VOC_burden <- function(
+#     para = NULL,
+#     rc_severity = c(1, 1.5,1.5), # relative change in ihr and ifr
+#     VE = NULL,
+# ){
+#   
+#   burden_processes_new <- 
+#     # mortality cases
+#     list(
+#       cm_multinom_process("E",       
+#                           data.frame(death_voc1 = P.death*rc_severity[1]),                   
+#                           delays = data.frame(death_voc1 = delay_2death), 
+#                           report = "o"),
+#       cm_multinom_process("E",       
+#                           data.frame(death_voc2 = P.death*prod(rc_severity[1:2])),                   
+#                           delays = data.frame(death_voc2 = delay_2death), 
+#                           report = "o"),
+#       cm_multinom_process("E",       
+#                           data.frame(death_voc3 = P.death*prod(rc_severity[1:3])),                   
+#                           delays = data.frame(death_voc3 = delay_2death), 
+#                           report = "o"),
+#       
+#       cm_multinom_process("Ev",      
+#                           data.frame(death_voc1 = P.death*(1-VE$ve_mort_condition[1])*(rc_severity[1])), 
+#                           delays = data.frame(death_voc1 = delay_2death), 
+#                           report = "o"),
+#       cm_multinom_process("Ev",      
+#                           data.frame(death_voc2 = P.death*(1-VE$ve_mort_condition[1])*prod(rc_severity[1:2])), 
+#                           delays = data.frame(death_voc2 = delay_2death), 
+#                           report = "o"),
+#       cm_multinom_process("Ev",      
+#                           data.frame(death_voc3 = P.death*(1-VE$ve_mort_condition[1])*prod(rc_severity[1:3])), 
+#                           delays = data.frame(death_voc3 = delay_2death), 
+#                           report = "o"),
+#       
+#       cm_multinom_process("Ev2",     
+#                           data.frame(death_voc1 = P.death*(1-VE$ve_mort_condition[2])*(rc_severity[1])), 
+#                           delays = data.frame(death_voc1 = delay_2death), 
+#                           report = "o"),
+#       cm_multinom_process("Ev2",     
+#                           data.frame(death_voc2 = P.death*(1-VE$ve_mort_condition[2])*prod(rc_severity[1:2])), 
+#                           delays = data.frame(death_voc2 = delay_2death),
+#                           report = "o"),
+#       cm_multinom_process("Ev2",
+#                           data.frame(death_voc3 = P.death*(1-VE$ve_mort_condition[2])*prod(rc_severity[1:3])), 
+#                           delays = data.frame(death_voc3 = delay_2death), 
+#                           report = "o"),
+#       
+#       # severe cases
+#       cm_multinom_process("E",       
+#                           data.frame(to_severe_voc1 = P.severe*(rc_severity[1]),
+#                                      to_critical_voc1 = P.critical*(rc_severity[1])),                  
+#                           delays = data.frame(to_severe_voc1 = delay_2severe,
+#                                               to_critical_voc1 = delay_2severe)),
+#       cm_multinom_process("E",       
+#                           data.frame(to_severe_voc2 = P.severe*prod(rc_severity[1:2]),
+#                                      to_critical_voc2 = P.critical*prod(rc_severity[1:2])),                  
+#                           delays = data.frame(to_severe_voc2 = delay_2severe,
+#                                               to_critical_voc2 = delay_2severe)),
+#       cm_multinom_process("E",       
+#                           data.frame(to_severe_voc3 = P.severe*prod(rc_severity[1:3]),
+#                                      to_critical_voc3 = P.critical*prod(rc_severity[1:3])),                  
+#                           delays = data.frame(to_severe_voc3 = delay_2severe,
+#                                               to_critical_voc3 = delay_2severe)),
+#       
+#       cm_multinom_process("Ev",      
+#                           data.frame(to_severe_voc1 = P.severe*(1-VE$ve_severe_condition[1])*(rc_severity[1]),
+#                                      to_critical_voc1 = P.critical*(1-VE$ve_critical_condition[1])*(rc_severity[1])),   
+#                           delays = data.frame(to_severe_voc1 = delay_2severe,
+#                                               to_critical_voc1 = delay_2severe)),
+#       cm_multinom_process("Ev",      
+#                           data.frame(to_severe_voc2 = P.severe*(1-VE$ve_severe_condition[1])*prod(rc_severity[1:2]),
+#                                      to_critical_voc2 = P.critical*(1-VE$ve_critical_condition[1])*prod(rc_severity[1:2])),   
+#                           delays = data.frame(to_severe_voc2 = delay_2severe,
+#                                               to_critical_voc2 = delay_2severe)),
+#       cm_multinom_process("Ev",      
+#                           data.frame(to_severe_voc3 = P.severe*(1-VE$ve_severe_condition[1])*prod(rc_severity[1:3]),
+#                                      to_critical_voc3 = P.critical*(1-VE$ve_critical_condition[1])*prod(rc_severity[1:3])),   
+#                           delays = data.frame(to_severe_voc3 = delay_2severe,
+#                                               to_critical_voc3 = delay_2severe)),
+#       
+#       cm_multinom_process("Ev2",     
+#                           data.frame(to_severe_voc1 = P.severe*(1-VE$ve_severe_condition[2])*(rc_severity[1]),
+#                                      to_critical_voc1 = P.critical*(1-VE$ve_critical_condition[2])*(rc_severity[1])),   
+#                           delays = data.frame(to_severe_voc1 = delay_2severe,
+#                                               to_critical_voc1 = delay_2severe)),
+#       cm_multinom_process("Ev2",     
+#                           data.frame(to_severe_voc2 = P.severe*(1-VE$ve_severe_condition[2])*prod(rc_severity[1:2]),
+#                                      to_critical_voc2 = P.critical*(1-VE$ve_critical_condition[2])*prod(rc_severity[1:2])),   
+#                           delays = data.frame(to_severe_voc2 = delay_2severe,
+#                                               to_critical_voc2 = delay_2severe)),
+#       cm_multinom_process("Ev2",     
+#                           data.frame(to_severe_voc3 = P.severe*(1-VE$ve_severe_condition[2])*prod(rc_severity[1:3]),
+#                                      to_critical_voc3 = P.critical*(1-VE$ve_critical_condition[2])*prod(rc_severity[1:3])),   
+#                           delays = data.frame(to_severe_voc3 = delay_2severe,
+#                                               to_critical_voc3 = delay_2severe)),
+#       
+#       cm_multinom_process("to_severe_voc1", data.frame(severe_voc1 = rep(1,16)),                          
+#                           delays = data.frame(severe_voc1 = delay_2hosp),   report = "ip"),
+#       cm_multinom_process("to_severe_voc2", data.frame(severe_voc2 = rep(1,16)),                          
+#                           delays = data.frame(severe_voc2 = delay_2hosp),   report = "ip"),
+#       cm_multinom_process("to_severe_voc3", data.frame(severe_voc3 = rep(1,16)),                          
+#                           delays = data.frame(severe_voc3 = delay_2hosp),   report = "ip"),
+#       
+#       cm_multinom_process("to_critical_voc1", data.frame(critical_voc1 = rep(1,16)),                          
+#                           delays = data.frame(critical_voc1 = delay_2hosp_critical),   report = "ip"),
+#       cm_multinom_process("to_critical_voc2", data.frame(critical_voc2 = rep(1,16)),                          
+#                           delays = data.frame(critical_voc2 = delay_2hosp_critical),   report = "ip"),
+#       cm_multinom_process("to_critical_voc3", data.frame(critical_voc3 = rep(1,16)),                          
+#                           delays = data.frame(critical_voc3 = delay_2hosp_critical),   report = "ip")
+#     )
+#   
+#   burden_updated <- c(para$processes, burden_processes_new)
+#   para$processes <- burden_updated
+#   
+#   return(para)
+# }
+
  
--- code/1_test.R
+++ code/1_test.R
@@ -4,27 +4,57 @@
 
 # require(tidyverse)
 
+source("code/0_LoadAll.R")
 
+compartment_labels <- c("S", "Sv_l", "Sv_m",
+                        "E", "Ev_l", "Ev_m",
+                        "Ip", "Ip_l", "Ip_m",
+                        "Is", "Is_l", "Is_m",
+                        "Ia", "Ia_l", "Ia_m",
+                        "R", "Rv_l", "Rv_m")
+outcome_labels <- c("cases", "severe_p", "critical_p", "severe_i", "critical_i", "death_o")
 
 #### Simple example ####
-params <- gen_country_basics("Thailand",
+params <- gen_country_basics(country = "Thailand",
+                             R0_assumed = 2.7,
+                             date_start = "2020-01-01",
+                             date_end = "2022-12-31",
+                             contact = contact_schedule,
+                             period_wn = 3*365,
+                             period_wv_ml = 1*365,
                              processes = burden_processes_az) %>% 
-  update_vac_char(., 
-                  ve_i = ve_az$ve_i_o[1],
+  update_u_y(para = ., 
+                  date_switch = c("2021-01-15", "2021-04-15", "2021-12-15"),
+                  rc_u = c(1, 1.5, 0.5), # relative changes in u
+                  rc_y = c(1, 0.5, 0.5), # relative changes in y
+                  rc_ve = c(1, 0.9, 0.7),
+                  efficacy_baseline = ve_az, 
+                  efficacy_weights = efficacy_weights_test
                   )
+
+# plot(x = params$schedule$yv_l_scaler$times,
+#      y = params$schedule$yv_l_scaler$values[[1]],
+#      type = "l")
 
 params$pop[[1]]$ur <- rep(0, 16)
 res <- cm_simulate(params)
 
 res$dynamics |>
-  filter(!compartment %in% c("cases", "cases_reported", "foi","foiv_l", "foiv_m", "subclinical")) |> 
-  # filter(compartment %in% c("S","E", "Ia","Ip","Is","R")) |> 
-  group_by(t, compartment) |> summarise(value = sum(value)) |> 
-  # group_by(t) |> 
-  # summarise(value = sum(value)) |> 
+  # filter(!compartment %in% c("cases", "cases_reported", "foi","foiv_l", "foiv_m", "subclinical")) |>
+  filter(compartment %in% compartment_labels) |>
+  group_by(t, compartment) |> summarise(value = sum(value)) |>
+  # group_by(t) |>
+  # summarise(value = sum(value)) |>
   # ggplot(aes(x = t, y = value)) + geom_line()
   ggplot(aes(x = t, y = value, group = compartment, color = compartment, fill = compartment)) +
   # geom_line() +
   geom_bar(position = "stack", stat = "identity")
+# 
+res$dynamics |>
+  # filter(!compartment %in% c("cases", "cases_reported", "foi","foiv_l", "foiv_m", "subclinical")) |>
+  filter(compartment %in% outcome_labels[c(1, 4:6)])|>
+  group_by(t, compartment) |> summarise(value = sum(value)) |>
+  ggplot(aes(x = t, y = value, group = compartment, color = compartment, fill = compartment)) +
+  geom_line() +
+  facet_wrap(~compartment, scales = "free")
 
-unique(res$dynamics$compartment)
