671243c5be1bff5b028c0a8a48aa4b4e1bbca7b0
Yang <yang.liu@lshtm.ac.uk>
2024-08-15 14:07:15 +0100

    add hitap version for comparison

--- code/1_fitting.R
+++ code/1_fitting.R
@@ -1,3 +1,4 @@
+source("code/0_LoadAll.R")
 source("code/1_1_fit_gen_country_basics.R")
 source("code/1_2_renew_fit_func.R")
 source("code/1_3_draw_fit.R")
@@ -33,19 +34,10 @@ for(i in 1:length(fvt)){
          filename = paste0("fit/fit_figures/", fvt[i], "_2.png"))
 }
 
-fit_gen_country_basics(country_tmp = "Thailand",
-                       country_code_tmp = "THA",
-                       date_start = params_tmp$fit_start,
-                       date_end = params_tmp$fit_end,
-                       R0_assumed = 2,
-                       period_wn = 3*365,
-                       period_wv_m2l = 1*365, 
-                       processes_set = burden_processes_all,
-                       period_wv_h2m = 1*365, 
-                       prob_v_p_2l = 0.33,
-                       prob_v_p_2m = 0.33,
-                       prob_v_b_l2m = 0,
-                       seed = 10,
-                       deterministic = TRUE)
-
-input_tmp <- c(1.2, 90, 0.1)
+# draw_fit(input = `fit_0.3`$optim$bestmem,
+#          country = "Thailand",
+#          draw_end = T,
+#          voc_features = voc_features_test %>% 
+#            mutate(change_u = 1),
+#          fit_vac_threshold = 0.1,
+#          dt_tmp = 0.3)
