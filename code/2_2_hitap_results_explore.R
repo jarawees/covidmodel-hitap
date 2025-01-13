b2a01997973ac5026e1216acae7d8dbd4e9e43f1
Jarawee Sukmanee <93040164+jarawees@users.noreply.github.com>
2025-01-13 10:06:50 +0700

    Update 2_2_hitap_results_explore.R
    extract results for manuscript

--- code/2_2_hitap_results_explore.R
+++ code/2_2_hitap_results_explore.R
@@ -3,7 +3,16 @@
 #   map(read_csv) %>% 
 #   setNames(fn) %>% 
 #   bind_rows(.id = "grid_table_index") 
-# 
+#
+# res %>%
+#   mutate(grid_table_index = gsub(".csv", "", grid_table_index)) %>%
+#   left_join(grid_table, by = "grid_table_index") %>%
+#   left_join(out_all %>%
+#               rowid_to_column(var = 'fit_table_index')) %>%
+#   left_join(panel_final %>%
+#               rowid_to_column(var = "panel_final_index")) %>%
+#   write_csv("20250118_Result for CUA.csv")
+#
 # res %<>% 
 #   mutate(grid_table_index = gsub(".csv", "", grid_table_index)) %>% 
 #   left_join(grid_table, by = "grid_table_index") %>% 
